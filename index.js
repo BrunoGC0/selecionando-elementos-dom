17b2e4669d890a76a21f7b2d947e84c48daf741e
Bruno Gonçalves <brunogcp28@gmail.com>
2024-06-21 12:52:52 -0400

    Aula 15 JS intermediário Selecionando elementos com DOM

---
+++ index.js
@@ -0,0 +1,19 @@
+console.log(document)
+
+let p = document.getElementById('paragrafo1')
+
+console.log(p)
+
+let inputsPorClass = document.getElementsByClassName('tag-de-input')
+
+
+console.log(inputsPorClass[0])
+
+
+let inputsPorTag = document.getElementsByTagName('h1')
+
+console.log(inputsPorTag)
+
+let inputName = document.querySelectorAll("input[name='email']")
+
+console.log(inputName[1])
